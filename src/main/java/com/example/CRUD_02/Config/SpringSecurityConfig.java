aa41ee2844397c3afffbf153c0874d5230fdff14
DhruvSingla007 <dhruvsingla07@gmail.com>
2023-04-21 19:59:42 +0530

    Added the JPA Method

--- src/main/java/com/example/CRUD_02/Config/SpringSecurityConfig.java
+++ src/main/java/com/example/CRUD_02/Config/SpringSecurityConfig.java
@@ -1,18 +1,17 @@
 package com.example.CRUD_02.Config;
 
+import com.example.CRUD_02.service.CustomUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -24,26 +23,25 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails userDetails1 = User.withUsername("dhruv")
-                .password(getPasswordEncoder().encode("dhruv"))
-                .roles("ADMIN")
-                .build();
+//        UserDetails userDetails1 = User.withUsername("dhruv")
+//                .password(getPasswordEncoder().encode("dhruv"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails userDetails2 = User.withUsername("anushka")
+//                .password(getPasswordEncoder().encode("anushka"))
+//                .roles("USER")
+//                .build();
 
-        UserDetails userDetails2 = User.withUsername("anushka")
-                .password(getPasswordEncoder().encode("anushka"))
-                .roles("USER")
-                .build();
 
-
-        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+        return new CustomUserDetailService();
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
@@ -70,6 +68,14 @@ public class SpringSecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+        return daoAuthenticationProvider;
     }
 
 }
